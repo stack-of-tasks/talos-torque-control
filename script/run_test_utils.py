dff5902367f2616544fe33663a02c2faad0992a6
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-07-28 12:30:37 +0200

    pre-commit run -a

--- script/run_test_utils.py
+++ script/run_test_utils.py
@@ -10,7 +10,8 @@ import rospy
 from std_srvs.srv import *
 from dynamic_graph_bridge_msgs.srv import *
 
-_runCommandClient = rospy.ServiceProxy('run_command', RunCommand)
+_runCommandClient = rospy.ServiceProxy("run_command", RunCommand)
+
 
 def runCommandClient(code):
     out = _runCommandClient(code)
@@ -19,39 +20,42 @@ def runCommandClient(code):
         if out.standardoutput:
             print("standardoutput: " + out.standardoutput)
         if out.standarderror:
-            print("standarderror: "  + out.standarderror)
+            print("standarderror: " + out.standarderror)
     return out
+
 
 def evalCommandClient(code):
     return eval(runCommandClient(code).result)
 
-def launch_script(code,title,description = ""):
-    raw_input(title+':   '+description)
+
+def launch_script(code, title, description=""):
+    raw_input(title + ":   " + description)
     rospy.loginfo(title)
     rospy.loginfo(code)
     for line in code:
-        if line != '' and line[0] != '#':
+        if line != "" and line[0] != "#":
             answer = runCommandClient(str(line))
             rospy.logdebug(answer)
-    rospy.loginfo("...done with "+title)
+    rospy.loginfo("...done with " + title)
+
 
 def run_test(appli):
     try:
         rospy.loginfo("Waiting for run_command")
-        rospy.wait_for_service('/run_command')
+        rospy.wait_for_service("/run_command")
         rospy.loginfo("...ok")
 
         rospy.loginfo("Waiting for start_dynamic_graph")
-        rospy.wait_for_service('/start_dynamic_graph')
+        rospy.wait_for_service("/start_dynamic_graph")
         rospy.loginfo("...ok")
 
-        runCommandStartDynamicGraph = rospy.ServiceProxy('start_dynamic_graph', Empty)
+        runCommandStartDynamicGraph = rospy.ServiceProxy("start_dynamic_graph", Empty)
 
-        initCode = open( appli, "r").read().split("\n")
+        initCode = open(appli, "r").read().split("\n")
 
         rospy.loginfo("Stack of Tasks launched")
 
-        launch_script(initCode,'initialize SoT')
+        launch_script(initCode, "initialize SoT")
         raw_input("Wait before starting the dynamic graph")
         runCommandStartDynamicGraph()
         print()
